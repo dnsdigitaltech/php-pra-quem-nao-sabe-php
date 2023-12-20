888851c84dbd3d8259175121db817552a2a87718
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-20 10:23:05 -0300

    Operadores de comparação

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 //operadores de comparacao
 
-$result = 30 == 50;
+$result = 30 == 50; //verifica se o valor é igual
 $result = 30 < 50;
 $result = 30 <= 50;
 $result = 30 > 50;
 $result = 30 >= 50;
-$result = 30 != 50; //Verifica se o valor e diferente
-$result = 30 === 50;
+$result = 30 != 50; //Verifica se o valor é diferente
+$result = 30 === 50; //verifica se o valor é igual e o tipo é igual
 $result = 30 !== 50;//verfica se o valor é diferente ou tipo e diferente
 
 
