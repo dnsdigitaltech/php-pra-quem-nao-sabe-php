338a014d1fb015adea913b68b590402f96180eac
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-20 17:09:02 -0300

    Loopings - Continue e break

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php
 
-//Loopings - Foreach
+//Loopings - Continue e break
 
 $names = ['Carlos', 'Luana', 'Joao'];
-//resgatar os valores utilizando Foreach
 
-//O Foreach significa para cada item do array vai ser guardado na variavel
+//Continue e break, quando quer pular ou verificar um valor e para naquele direção
+
+// for ($i=0; $i < count($names); $i++) { 
+//     //pular Luana
+//     if($i === 1){
+//         continue;
+//     }
+
+//     // //parar ao chegar na Luana
+//     // if($i === 1){
+//     //     break;
+//     // }
+//     echo $names[$i];
+// }
 
 foreach ($names as $key => $name) {
-    echo $key . '=>' . $name;
+    if($key === 1){
+        continue;
+    }
+    echo $name;
 }
+
+//OBS no while não funciona o continue a break funciona
