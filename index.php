7ba1eda325589946257ba877afe374c257474cb0
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-20 09:37:13 -0300

    Variáveis, seus tipos de dados e referência

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php
-include './teste.php';
+//Variaveis PHP temos:
+
+//string
+echo gettype('kkkkk'). '<br>';
+
+//numbers - integers, float(double)
+echo gettype(12). '<br>';
+echo gettype(12.3). '<br>';
+
+//booleans
+echo gettype(false). '<br>';
+
+//arrays
+echo gettype(['kjshkjsdf', 12]). '<br>';
+
+//objects
+class Person{
+
+}
+echo gettype(new Person). '<br>';
+
+//null
+echo gettype(null). '<br>';
+
+//Criando variável
+
+$name = 'Davi';
+echo $name. '<br>';
+echo gettype($name). '<br>';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title><?=$title?></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-        <h1>Olá, <?=$name;?></h1>
-    </body>
-</html>
