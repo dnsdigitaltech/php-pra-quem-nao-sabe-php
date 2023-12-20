70864e6decd483be43305d717b508c5092e7529f
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-20 16:43:23 -0300

    Loopings - For

--- index.php
+++ index.php
@@ -1,48 +1,12 @@
 <?php
 
-//Convertendo tipos de dados
-//(string)
-//(boolean)
-//(float)
-//(integer)
-//(double)
-//(object)
-//(array)
-//intVal
-//floatVal
-//doubleVal
-//strVal
-//boolVal
+//Loopings - For
 
-$name = 'Davi';
-//$name = '';false
-$number = 35;
-//converter para boolean
-$change = (boolean) $name;
-$change = boolVal($name);//este método é outra forma de chamar mais o resultado é o mesmo
-$number = (string) $number;
-var_dump($change, $number);
+$names = ['Carlos', 'Luana', 'Joao'];
+//resgatar os valores utilizando for
 
-//OBS para converter oa valores basta colocar o tipo do dado antes da variavel
+//O for efetua o lopping e pós incrementa
 
-//Transformar array em object
-$nomes = ['name' => 'Davi', 'age' => 39];
-
-$object = (object) $nomes;
-var_dump($object);
-
-//recuperar objeto
-var_dump($object->name);
-
-//Transformar objects e array
-class Person{
-    private $name;
-    public function teste() {
-        return 'teste';
-    }
+for ($i=0; $i < count($names); $i++) { 
+   echo $names[$i].'<br>';
 }
-
-$person = new Person;
-
-$array = (array) $person;
-var_dump($array);
