583603d194d5b5cfc35a912a8b3ead9f45350c85
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-21 10:54:00 -0300

    Variáveis de ambiente

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
+require './vendor/autoload.php';
+//Variaveis de ambiente
 
-//Session
-//session_start() - sempre quando for criar a sessão
-session_start();
-//resgatando o id da sessão
-echo session_id();
-$_SESSION['name'] = 'Davi';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-//OBS: para excluir a sessão basta fechar o navegador ou excluir manualmente
-//Para acluir um item na sessão basta usar a função unset()
-//unset($_SESSION['name']);
-
-//session_destroy() - exclui toda a sessão
-session_destroy();
-
-include './teste.php';
+var_dump($_ENV['KEY']);
--- teste.php
+++
@@ -1,5 +0,0 @@
-<?php
-//sempre quando for resgatar uma sessão tem que iniciar
-session_start();
-//resgatando a sessão
-echo isset($_SESSION['name']) ? $_SESSION['name'] :'Sessão não existe'; //operador ternário
