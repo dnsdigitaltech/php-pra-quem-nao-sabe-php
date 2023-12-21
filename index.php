2eda16218561ef9c5d12d43afbd70ed796276ac6
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-21 11:03:40 -0300

    Variáveis do servidor

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 require './vendor/autoload.php';
-//Variaveis de ambiente
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//Variaveis do servidor
+//__DIR__  == $_SERVER['DOCUMENT_ROOT'] - pega o caminho completo do diretorio do projeto
+//echo __DIR__;
+$dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
 $dotenv->load();
 
-var_dump($_ENV['KEY']);
+var_dump($_ENV['DATABASE']);
