8298075981ae643a4b7905e6ad1717cc5c8d7650
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-21 09:32:42 -0300

    Introdução sobre as variáveis super globais

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
 
-//Escopo e variável global
+//Variáveis super globais
+//$_ e o nome da super global, todas com letras em Maiúsculas
 
-//global - é em todo script
-//local - dentro de uma função
+//$_COOKIE - É um valor guardado no navegador, podendo ser resgatado em qualquer parte do sistema. Ele possui um prazo de  validade. Dentro dele podemos guardar os mais variados dados possíveis.
 
-$name = 'Davi';
+//$_SESSION - Também é guardada no navegador, porém não é guardado o valor da sessão
 
-function person(){
-    //global $name;
-    //echo $name;
+//$_ENV - é um array associativo onde cada uma das suas chaves é uma variável de ambiente apontando para o seu valor.
 
-    //OU
+//$_FILES - pegar arquivos enviado de formulários
 
-    echo $GLOBALS['name'];
-}
+//$_GET - pega os dado via URL (query string)
 
-person();
+//$_POST - pega os valores enviado através do header
+
+//$_REQUEST - pega tanto query string ($_GET), quanto valores enviado através do header($_POST)
+
+//$_SERVER - pega a url, a raiz do projeto, tipo de instrução($_GET, $_POST)
