34a1b74b909c4c19ad6e67e1dfbc036ccb5f6964
Davi Bernardo <davi.bernardo@sbahq.org>
2023-12-21 11:21:05 -0300

    GET e POST

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<?php
-require './vendor/autoload.php';
-//Variaveis do servidor
-//__DIR__  == $_SERVER['DOCUMENT_ROOT'] - pega o caminho completo do diretorio do projeto
-//echo __DIR__;
-$dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT']);
-$dotenv->load();
-
-var_dump($_ENV['DATABASE']);
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Curso de PHP</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <form action="teste.php" method="post">
+            <input type="text" name="name" value="" placeholder="Seu nome">
+            <input type="email" name="email" value="" placeholder="Seu e-mail">
+            <button type="submit">Cadastrar</button>
+        </form>
+    </body>
+</html>
---
+++ teste.php
@@ -0,0 +1,10 @@
+<?php
+
+// var_dump($_POST);
+// var_dump($_POST['name']);
+
+// var_dump($_GET);
+// var_dump($_GET['name']);
+
+var_dump($_REQUEST);
+var_dump($_REQUEST['name']);
